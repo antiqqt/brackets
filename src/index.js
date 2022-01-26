ec0c805f2eeae2957b0e3a85477323974e345c31
Anton <antonpomogaev@gmail.com>
2022-01-26 14:40:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pairs = {}; // First, we make hashtable to keep our brackets pairs;
+  bracketsConfig.forEach((item) => {
+    pairs[item[0]] = item[1];
+  });
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const elem = str[i];
+
+    // Check for same brackets;
+    if (pairs[elem] === elem && stack[stack.length - 1] === elem) {
+      stack.pop();
+      continue;
+    }
+
+    // Check for ordinary brackets;
+    if (pairs[elem]) {
+      stack.push(elem);
+    } else {
+      if (elem !== pairs[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
+};
